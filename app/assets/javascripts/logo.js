ed6fa928ec062851b9f73a06f154044ec19ec668
Vicente Lucas <vica.escorcio@gmail.com>
2016-12-09 13:53:02 -0300

    fade in effect change, slider removed

--- app/assets/javascripts/logo.js
+++ app/assets/javascripts/logo.js
@@ -2,7 +2,7 @@ $(window).ready(function(){
  //$('.logo').animate({'margin-left':'400px'},3000);
     $(".button-collapse").sideNav();
     $('.slider').slider({full_width: true});
-    $('.loading').hide(5000,function(){ $(this).remove(); });
-    $(".div1").fadeIn(6000);
+    $('.loading').hide(8000,function(){ $(this).remove(); });
+    $(".div1").fadeIn(9000);
 });
 
