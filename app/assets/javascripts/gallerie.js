0f9855089f52bb4a9a0a8a8b8b019be6b0de3ab6
Vicente Lucas <vica.escorcio@gmail.com>
2017-02-02 12:44:56 -0300

    a

---
+++ app/assets/javascripts/gallerie.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    $('#imageGallery').lightSlider({
+        gallery:true,
+        item:1,
+        loop:true,
+        thumbItem:9,
+        slideMargin:0,
+        enableDrag: false,
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#imageGallery .lslide'
+            });
+        }   
+    });  
+  });
